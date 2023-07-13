4428d9ab9ef88ff106089e98b2476ab9bd3f711b
Jean Carlo <jeancarlodev@gmail.com>
2023-07-13 13:02:48 -0300

    add eslint

--- src/shared/domain/repository/__tests__/in-memory-searchable.repository.test.ts
+++ src/shared/domain/repository/__tests__/in-memory-searchable.repository.test.ts
@@ -1,71 +1,143 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Entity } from "../../../../shared/domain/entity/entity";
 import { inMemorySearchableRepository } from "../in-memory.repository";
 
 type StubEntityProps = {
-  name: string;
-  price: number;
+    name: string;
+    price: number;
 };
 
 class StubEntity extends Entity<StubEntityProps> {}
 
 class StubInMemorySearchableRepository extends inMemorySearchableRepository<StubEntity> {
-  sortableFields: string[] = ["name"];
-  protected async applyFilter(items: StubEntity[], filter: string): Promise<StubEntity[]> {
-    if (!filter) {
-      return items;
-    }
+    sortableFields: string[] = ["name"];
+    protected async applyFilter(
+        items: StubEntity[],
+        filter: string,
+    ): Promise<StubEntity[]> {
+        if (!filter) {
+            return items;
+        }
 
-    return items.filter((i) => {
-      return i.props.name.toLowerCase().includes(filter.toLowerCase()) || i.props.price.toString() === filter;
-    });
-  }
+        return items.filter((i) => {
+            return (
+                i.props.name.toLowerCase().includes(filter.toLowerCase()) ||
+                i.props.price.toString() === filter
+            );
+        });
+    }
 }
 
 describe("InMemorySearchableRepository", () => {
-  let repository: StubInMemorySearchableRepository;
+    let repository: StubInMemorySearchableRepository;
 
-  beforeEach(() => {
-    repository = new StubInMemorySearchableRepository();
-  });
-  describe("applyFilter method", () => {
-    it("should no filter items when filter param is null", async () => {
-      const items = [new StubEntity({ name: "name value", price: 5 })];
-      const spyFilterMethod = jest.spyOn(items, "filter" as any);
-      const itemsFiltered = await repository["applyFilter"](items, null);
+    beforeEach(() => {
+        repository = new StubInMemorySearchableRepository();
+    });
+    describe("applyFilter method", () => {
+        it("should no filter items when filter param is null", async () => {
+            const items = [new StubEntity({ name: "name value", price: 5 })];
+            const spyFilterMethod = jest.spyOn(items, "filter" as any);
+            const itemsFiltered = await repository["applyFilter"](items, null);
 
-      expect(itemsFiltered).toStrictEqual(items);
+            expect(itemsFiltered).toStrictEqual(items);
 
-      expect(spyFilterMethod).not.toHaveBeenCalled();
+            expect(spyFilterMethod).not.toHaveBeenCalled();
+        });
+
+        it("should filter using a filter param", async () => {
+            const items = [
+                new StubEntity({ name: "test", price: 5 }),
+                new StubEntity({ name: "TEST", price: 5 }),
+                new StubEntity({ name: "fake", price: 0 }),
+            ];
+
+            const spyFilterMethod = jest.spyOn(items, "filter" as any);
+
+            let itemsFiltered = await repository["applyFilter"](items, "TEST");
+
+            expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+
+            expect(spyFilterMethod).toHaveBeenCalled();
+
+            itemsFiltered = await repository["applyFilter"](items, "5");
+
+            expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+
+            expect(spyFilterMethod).toHaveBeenCalledTimes(2);
+
+            itemsFiltered = await repository["applyFilter"](items, "no-filter");
+
+            expect(itemsFiltered).toHaveLength(0);
+
+            expect(spyFilterMethod).toHaveBeenCalledTimes(3);
+        });
     });
 
-    it("should filter using a filter param", async () => {
-      let items = [new StubEntity({ name: "test", price: 5 }), new StubEntity({ name: "TEST", price: 5 }), new StubEntity({ name: "fake", price: 0 })];
+    describe("applySort method", () => {
+        it("should no sort items", async () => {
+            const items = [
+                new StubEntity({ name: "b", price: 5 }),
+                new StubEntity({ name: "a", price: 5 }),
+            ];
 
-      const spyFilterMethod = jest.spyOn(items, "filter" as any);
+            let itemsSorted = await repository["applySort"](items, null, null);
 
-      let itemsFiltered = await repository["applyFilter"](items, "TEST");
+            expect(itemsSorted).toStrictEqual(items);
 
-      expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+            itemsSorted = await repository["applySort"](items, "price", "asc");
 
-      expect(spyFilterMethod).toHaveBeenCalled();
+            expect(itemsSorted).toStrictEqual(items);
+        });
 
-      itemsFiltered = await repository["applyFilter"](items, "5");
+        it("should  sort items", async () => {
+            const items = [
+                new StubEntity({ name: "b", price: 5 }),
+                new StubEntity({ name: "a", price: 5 }),
+                new StubEntity({ name: "c", price: 0 }),
+            ];
 
-      expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+            let itemsSorted = await repository["applySort"](
+                items,
+                "name",
+                "asc",
+            );
 
-      expect(spyFilterMethod).toHaveBeenCalledTimes(2);
+            expect(itemsSorted).toStrictEqual([items[1], items[0], items[2]]);
 
-      itemsFiltered = await repository["applyFilter"](items, "no-filter");
+            itemsSorted = await repository["applySort"](items, "name", "desc");
 
-      expect(itemsFiltered).toHaveLength(0);
-
-      expect(spyFilterMethod).toHaveBeenCalledTimes(3);
+            expect(itemsSorted).toStrictEqual([items[2], items[0], items[1]]);
+        });
     });
-  });
 
-  describe("applySort method", () => {});
+    describe("applyPaginate method", () => {
+        it("should  paginate items", async () => {
+            const items = [
+                new StubEntity({ name: "b", price: 5 }),
+                new StubEntity({ name: "a", price: 5 }),
+                new StubEntity({ name: "c", price: 0 }),
+                new StubEntity({ name: "d", price: 0 }),
+                new StubEntity({ name: "e", price: 0 }),
+            ];
 
-  describe("applyPaginatemethod", () => {});
+            let itemsPaginated = await repository["applyPaginate"](items, 1, 2);
 
-  describe("Search method", () => {});
+            expect(itemsPaginated).toStrictEqual([items[0], items[1]]);
+
+            itemsPaginated = await repository["applyPaginate"](items, 2, 2);
+
+            expect(itemsPaginated).toStrictEqual([items[2], items[3]]);
+
+            itemsPaginated = await repository["applyPaginate"](items, 3, 2);
+
+            expect(itemsPaginated).toStrictEqual([items[4]]);
+
+            itemsPaginated = await repository["applyPaginate"](items, 4, 2);
+
+            expect(itemsPaginated).toStrictEqual([]);
+        });
+    });
+
+    describe("Search method", () => {});
 });
