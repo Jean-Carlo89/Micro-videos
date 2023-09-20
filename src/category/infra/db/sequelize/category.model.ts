2a2b58bdec939fe7fccf13f1244880baca199c47
Jean Carlo <jeancarlodev@gmail.com>
2023-09-20 10:03:51 -0300

    creeating sequelize repository and tests

--- src/category/infra/db/sequelize/category.model.ts
+++ src/category/infra/db/sequelize/category.model.ts
@@ -1,14 +1,23 @@
-import { Column, Model, DataType, Table } from "sequelize-typescript";
+import { Column, Model, DataType, Table, PrimaryKey } from "sequelize-typescript";
+
+export type CategoryModelProps = {
+  category_id: string;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
+};
 
 @Table({ tableName: "categories", timestamps: false })
-export class CategoryModel extends Model {
+export class CategoryModel extends Model<CategoryModelProps> {
+  @PrimaryKey
   @Column({ type: DataType.UUID })
   declare category_id: string;
 
   @Column({ allowNull: false, type: DataType.STRING(255) })
   declare name: string;
 
-  @Column({ allowNull: true, type: DataType.STRING(255) })
+  @Column({ allowNull: true, type: DataType.TEXT })
   declare description: string | null;
 
   @Column({ allowNull: false, type: DataType.BOOLEAN })
@@ -17,3 +26,17 @@ export class CategoryModel extends Model {
   @Column({ allowNull: false, type: DataType.DATE(3) })
   declare created_at: Date;
 }
+
+// interface CategoryModelInterface {
+//   category_id: string;
+
+//   // test: boolean;
+
+//   name: string;
+
+//   description: string | null;
+
+//   is_active: boolean;
+
+//   created_at: Date;
+// }
