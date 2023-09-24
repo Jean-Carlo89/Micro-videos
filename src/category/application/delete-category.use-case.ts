ddd61bcd0ffd7d299b203e0d2fde30d2595724b2
Jean Carlo <jeancarlodev@gmail.com>
2023-09-24 17:03:58 -0300

    add update category use case and tests

---
+++ src/category/application/delete-category.use-case.ts
@@ -0,0 +1,18 @@
+import { IUseCase } from "../../shared/application/use-case.interface";
+import { ICategoryRepository } from "../domain/category.repository";
+import { Uuid } from "../../shared/domain/value-object/uuid.vo";
+
+export class DeleteCategoryUseCase implements IUseCase<DeleteCategoryInput, DeleteCategoryOutput> {
+  constructor(private categoryRepo: ICategoryRepository) {}
+
+  async execute(input: DeleteCategoryInput): Promise<DeleteCategoryOutput> {
+    const uuid = new Uuid(input.id);
+    await this.categoryRepo.delete(uuid);
+  }
+}
+
+export type DeleteCategoryInput = {
+  id: string;
+};
+
+type DeleteCategoryOutput = void;
