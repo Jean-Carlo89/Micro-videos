6f63e458502e471fc8ee8fdd77b130874c8a9bac
Jean Carlo <jeancarlodev@gmail.com>
2023-11-10 15:14:44 -0300

    feat: add tests for config and db modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,16 @@ import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './database-module/database.module';
 
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from './config-module/config.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
     CategoriesModule,
-    ConfigModule,
+
     // SequelizeModule.forFeature([]),
   ],
   controllers: [AppController],
